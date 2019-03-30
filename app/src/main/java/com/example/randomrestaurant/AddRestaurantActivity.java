f8fa65caa72317676ff6028a7c0a650869adb0f4
MarjanGit <mansar16@huntersoe.org>
2019-03-29 21:29:32 -0400

    doesnt crash and added database instance but not functional

--- app/src/main/java/com/example/randomrestaurant/AddRestaurantActivity.java
+++ app/src/main/java/com/example/randomrestaurant/AddRestaurantActivity.java
@@ -5,23 +5,100 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 import android.widget.TextView;
 
-public class AddRestaurantActivity extends Activity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-//    FavoritesListActivity my_faves_list = new FavoritesListActivity();
+import java.util.ArrayList;
+import java.util.List;
 
-    private EditText editRestaurant;
+public class AddRestaurantActivity extends AppCompatActivity {
+    //view objects
+    EditText editTextName;
+    Spinner spinnerRestaurant;
+    Button buttonAddRestaurant;
+    ListView listViewRestaurants;
+
+    //a list to store all the restaurant from firebase database
+    List<String> restaurants;
+
+    //our database reference object
+    DatabaseReference databaseRestaurants;
+//    Spinner spinnerRestaurant;
+//    EditText editRestaurant;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_restaurant);
-//        editRestaurant = (EditText) findViewById(R.id.editRestaurant);
-//        String toAdd = editRestaurant.getText().toString().trim();
-//        my_faves_list.addRestaurant(toAdd );
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_add_restaurant);
+
+        //getting the reference of artists node
+        databaseRestaurants = FirebaseDatabase.getInstance().getReference("restaurants");
+
+        //getting views
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        spinnerRestaurant = (Spinner) findViewById(R.id.spinnerRestaurant);
+        listViewRestaurants = (ListView) findViewById(R.id.listViewRestaurants);
+        buttonAddRestaurant = (Button) findViewById(R.id.buttonAddRestaurant);
+
+        //list to store artists
+        restaurants = new ArrayList<>();
+
+
+        //adding an onclicklistener to button
+        buttonAddRestaurant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //calling the method addArtist()
+                //the method is defined below
+                //this method is actually performing the write operation
+                addRestaurant();
+            }
+        });
     }
 
-    public void gotoAdd(View view){
-        // Second view when click the button of the main view
-        startActivity(new Intent(this, FavoritesListActivity.class));
+    /*
+     * This method is saving a new artist to the
+     * Firebase Realtime Database
+     * */
+    private void addRestaurant() {
+        //getting the values to save
+        String name = editTextName.getText().toString().trim();
+
+        //checking if the value is provided
+        if (!TextUtils.isEmpty(name)) {
+
+            //getting a unique id using push().getKey() method
+            //it will create a unique id and we will use it as the Primary Key for our Artist
+            String id = databaseRestaurants.push().getKey();
+
+
+            //Saving the Artist
+            databaseRestaurants.child(id).setValue(name);
+
+            //setting edittext to blank again
+            editTextName.setText("");
+
+            //displaying a success toast
+            Toast.makeText(this, "Restaurant added", Toast.LENGTH_LONG).show();
+        } else {
+            //if the value is not given displaying a toast
+            Toast.makeText(this, "Please enter a name", Toast.LENGTH_LONG).show();
+        }
     }
 }
+
+
