b6e4cfe204bc47729eb9bbdd8481174c999c0194
MarjanGit <mansar16@huntersoe.org>
2019-04-01 16:23:40 -0400

    god bless

--- app/src/main/java/com/example/randomrestaurant/MainActivity.java
+++ app/src/main/java/com/example/randomrestaurant/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,11 +17,13 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "Register";
     private EditText editTextEmail;
     private EditText editTextPassword;
     private Button buttonSignup;
@@ -35,9 +38,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //FirebaseApp.initializeApp(this);
+
         progressDialog = new ProgressDialog(this);
 
         firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser() != null){
+            //close this activity
+            finish();
+            //opening lost activity
+            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+        }
+
         buttonSignup = (Button) findViewById(R.id.buttonSignup);
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
@@ -45,11 +58,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonSignup.setOnClickListener(this);
         textViewSignin.setOnClickListener(this);
 
-
-
     }
 
-    private void RegisterUser() {
+    private void registerUser() {
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
 
@@ -61,21 +72,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT).show();
             return;
         }
+        Log.d(TAG, "Got password and email");
         progressDialog.setMessage("Registering Please Wait...");
         progressDialog.show();
-
+        Log.d(TAG, "Got password and email");
         firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 //checking if success
+                progressDialog.dismiss();
+                Log.d(TAG, "Making");
                 if(task.isSuccessful()){
+                    Log.d(TAG, "ENTERED SUCCESS");
                     finish();
                     startActivity(new Intent(getApplicationContext(), HomeActivity.class));
                 }else{
                     //display some message here
                     Toast.makeText(MainActivity.this,"Registration Error",Toast.LENGTH_LONG).show();
+                    Log.d(TAG, "Failed");
                 }
-                progressDialog.dismiss();
             }
         });
     }
@@ -83,7 +98,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view){
         if(view == buttonSignup){
-            RegisterUser();
+            registerUser();
         }
         if(view == textViewSignin){
             startActivity(new Intent(this, LoginActivity.class));
